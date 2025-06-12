94cfba1b608a8e4bc405dbc5bccb567ff43be396
0xull <mrikehchukwuka@gmail.com>
2025-06-12 04:09:59 +0100

    buggy(singly-linked queue): preserving this checkpoint for learning sake

--- src/fifth.rs
+++ src/fifth.rs
@@ -31,4 +31,48 @@ impl<'a, T> List<'a, T> {
         
         self.tail = new_tail;
     }
+    
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            if self.head.is_none() {
+                self.tail = None;
+            }
+            
+            node.elem
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    fn basics() {
+        let mut list = List::new();
+        
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+        
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        
+        // Check normal removal
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+        
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+        
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+        
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+    }
 }
