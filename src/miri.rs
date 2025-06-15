5df5edff8620e3a0484278a6312e9839ab1d2922
0xull <mrikehchukwuka@gmail.com>
2025-06-15 22:57:27 +0100

    test(stacked borrows): explored the combined use of mutable reference, raw pointers, unsafecell and stacked borrows

--- src/miri.rs
+++ src/miri.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod test {
+    use std::cell::{Cell, UnsafeCell};
+
     #[test]
     pub fn miri() {
         let mut data = 10;
@@ -162,6 +164,86 @@ mod test {
             }
             
             println!("{:?}", &data[..])
+        }
+    }
+    
+    
+    fn opaque_read(val: &i32) {
+        println!("{:}", val);
+    }
+    
+    #[test]
+    fn miri1() {
+        let mut data = 10;
+        let mref1 = &mut data;
+        let sref2 = &mref1;
+        let sref3 = sref2;
+        let sref4 = &*sref3;
+        
+        opaque_read(sref3);
+        opaque_read(sref2);
+        opaque_read(sref4);
+        opaque_read(sref2);
+        opaque_read(sref3);
+        
+        *mref1 += 1;
+        
+        opaque_read(&mref1);
+        
+        unsafe {
+            let mut data = 10;
+            let mref1 = &mut data;
+            let ptr2 = mref1 as *mut _;
+            let sref3 = &*mref1;
+            let ptr4 = sref3 as *const _ as *mut i32;
+            
+            
+            opaque_read(&*ptr4);
+            opaque_read(sref3);
+            *ptr2 += 2;
+            *mref1 += 1;
+            
+            opaque_read(&data);
+        }
+        
+        unsafe {
+            let mut data = Cell::new(10);
+            let mref1 = &mut data;
+            let ptr2 = mref1 as *mut Cell<i32>;
+            let sref3 = &*mref1;
+            
+            sref3.set(sref3.get() + 3);
+            (*ptr2).set((*ptr2).get() + 2);
+            mref1.set(mref1.get() + 1);
+            
+            println!("{:}", data.get());
+        }
+        
+        unsafe {
+            let mut data = UnsafeCell::new(10);
+            let mref1 = data.get_mut();
+            let ptr2 = mref1 as *mut _;
+            let sref3 = &*mref1;
+            
+            *ptr2 += 2;
+            opaque_read(sref3);
+            *mref1 += 1;
+            
+            println!("{:}", *data.get());
+        }
+        
+        unsafe {
+            let mut data = UnsafeCell::new(10);
+            let mref1 = &mut data;
+            let sref2 = &*mref1;
+            let ptr3 = sref2.get();
+            
+            *ptr3 += 3;
+            opaque_read(&*sref2.get());
+            *sref2.get() += 3;
+            *mref1.get() += 1;
+            
+            println!("{:}", *data.get());
         }
     }
 }
