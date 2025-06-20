2f2cc54eb331aa7ba66208ca7ca333803655cabc
0xull <mrikehchukwuka@gmail.com>
2025-06-20 05:10:55 +0100

    feat(doubly-linked deque): add front push and pop ops implementation

--- src/prod.rs
+++ src/prod.rs
@@ -1,13 +1,116 @@
-pub struct List<T> {
+use std::{marker::PhantomData, ptr::NonNull};
+
+pub struct LinkedList<T> {
     front: Link<T>,
     back: Link<T>,
-    len: usize
+    len: usize,
+    _boo: PhantomData<T>,
 }
 
-type Link<T> = *mut Node<T>;
+type Link<T> = Option<NonNull<Node<T>>>;
 
 struct Node<T> {
     elem: T,
     front: Link<T>,
-    back: Link<T>
+    back: Link<T>,
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList {
+            front: None,
+            back: None,
+            len: 0,
+            _boo: PhantomData,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        unsafe {
+            let new = NonNull::new_unchecked(Box::into_raw(Box::new(Node {
+                elem: elem,
+                front: None,
+                back: None,
+            })));
+            if let Some(old) = self.front {
+                (*old.as_ptr()).front = Some(new);
+                (*new.as_ptr()).back = Some(old);
+            } else {
+                debug_assert!(self.front.is_none());
+                debug_assert!(self.back.is_none());
+                debug_assert!(self.len == 0);
+                self.back = Some(new);
+            }
+            self.front = Some(new);
+            self.len += 1;
+        }
+    }
+    
+    pub fn pop_front(&mut self) -> Option<T> {
+        unsafe {
+            self.front.map(|node| {
+                let boxed_node = Box::from_raw(node.as_ptr());
+                let result = boxed_node.elem;
+                self.front = boxed_node.back;
+                
+                if let Some(new) = self.front {
+                    (*new.as_ptr()).front = None;
+                } else {
+                    debug_assert!(self.len == 1);
+                    self.back = None;
+                }
+                self.len -= 1;
+                result
+            })
+        }
+    }
+    
+    pub fn len(&self) -> usize {
+        self.len
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::LinkedList;
+
+    #[test]
+    fn test_basic_front() {
+        let mut list = LinkedList::new();
+
+        // Try to break an empty list
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+
+        // Try to break a one item list
+        list.push_front(10);
+        assert_eq!(list.len(), 1);
+        assert_eq!(list.pop_front(), Some(10));
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+
+        // Mess around
+        list.push_front(10);
+        assert_eq!(list.len(), 1);
+        list.push_front(20);
+        assert_eq!(list.len(), 2);
+        list.push_front(30);
+        assert_eq!(list.len(), 3);
+        assert_eq!(list.pop_front(), Some(30));
+        assert_eq!(list.len(), 2);
+        list.push_front(40);
+        assert_eq!(list.len(), 3);
+        assert_eq!(list.pop_front(), Some(40));
+        assert_eq!(list.len(), 2);
+        assert_eq!(list.pop_front(), Some(20));
+        assert_eq!(list.len(), 1);
+        assert_eq!(list.pop_front(), Some(10));
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+    }
 }
